60e735560b70eb2598fd3e5ef220c6e99bd69caa
Michelle-l33 <michelle.lee@spartans.ut.edu>
2025-01-30 10:17:14 -0500

    React Framework

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from './Components/home';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
